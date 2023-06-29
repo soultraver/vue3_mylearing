0de913c3a842ed560702268cd5d26be8ae096068
soultraver <soultraver@163.com>
2023-06-29 15:11:59 +0800

    HelloWorld.vue文件修改

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -14,6 +14,8 @@ defineProps({
       You’ve successfully created a project with
       <a href="https://vitejs.dev/" target="_blank" rel="noopener">Vite</a> +
       <a href="https://vuejs.org/" target="_blank" rel="noopener">Vue 3</a>.
+      <br />
+      你很棒!
     </h3>
   </div>
 </template>
